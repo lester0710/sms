4b4242ce6efb9225159de7426384e464647bbc17
lester0710 <98529564+lester0710@users.noreply.github.com>
2022-01-27 21:17:50 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
-<?php require_once 'admin/db_con.php'; ?>
+<?php require_once 'db_con.php';
+session_start();
+if (!isset($_SESSION['user_login'])) {
+  header('Location: login.php');
+}
+?>
 <!doctype html>
 <html lang="en">
   <head>
@@ -7,129 +12,81 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="../css/fontawesome.min.css">
+    <link rel="stylesheet" href="../css/solid.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.min.css"/>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <title>SMS</title>
-  </head>
-  <body>
-    <div class="container"><br>
-      <a class="btn btn-primary float-right" href="admin/login.php">Login</a>
-          <h1 class="text-center">Welcome to Student Management System!</h1><br>
-
-          <div class="row">
-            <div class="col-md-4 offset-md-4">
-              <form method="POST">
-            <table class="text-center infotable">
-              <tr>
-                <th colspan="2">
-                  <p class="text-center">Student Information</p>
-                </th>
-              </tr>
-              <tr>
-                <td>
-                   <p>Choose Class</p>
-                </td>
-                <td>
-                   <select class="form-control" name="choose">
-                     <option value="">
-                       Select
-                     </option>
-                     <option value="1st">
-                       1St
-                     </option>
-                     <option value="2nd">
-                       2nd
-                     </option>
-                     <option value="3rd">
-                       3rd
-                     </option>
-                     <option value="4th">
-                       4th
-                     </option>
-                     <option value="5th">
-                       5th
-                     </option>
-                   </select>
-                </td>
-              </tr>
-
-              <tr>
-                <td>
-                  <p><label for="roll">Roll No</label></p>
-                </td>
-                <td>
-                  <input class="form-control" type="text" pattern="[0-9]{6}" id="roll" placeholder="Roll Num.." name="roll">
-                </td>
-              </tr>
-              <tr>
-                <td colspan="2" class="text-center">
-                  <input class="btn btn-danger" type="submit" name="showinfo">
-                </td>
-              </tr>
-            </table>
-          </form>
-            </div>
-          </div>
-        <br>
-        <?php if (isset($_POST['showinfo'])) {
-          $choose= $_POST['choose'];
-          $roll = $_POST['roll'];
-          if (!empty($choose && $roll)) {
-            $query = mysqli_query($db_con,"SELECT * FROM `student_info` WHERE `roll`='$roll' AND `class`='$choose'");
-            if (!empty($row=mysqli_fetch_array($query))) {
-              if ($row['roll']==$roll && $choose==$row['class']) {
-                $stroll= $row['roll'];
-                $stname= $row['name'];
-                $stclass= $row['class'];
-                $city= $row['city'];
-                $photo= $row['photo'];
-                $pcontact= $row['pcontact'];
-              ?>
-        <div class="row">
-          <div class="col-sm-6 offset-sm-3">
-            <table class="table table-bordered">
-              <tr>
-                <td rowspan="5"><h3>Student Info</h3><img class="img-thumbnail" src="admin/images/<?= isset($photo)?$photo:'';?>" width="250px"></td>
-                <td>Name</td>
-                <td><?= isset($stname)?$stname:'';?></td>
-              </tr>
-              <tr>
-                <td>Roll</td>
-                <td><?= isset($stroll)?$stroll:'';?></td>
-              </tr>
-              <tr>
-                <td>Class</td>
-                <td><?= isset($stclass)?$stclass:'';?></td>
-              </tr>
-              <tr>
-                <td>City</td>
-                <td><?= isset($city)?$city:'';?></td>
-              </tr>
-              <tr>
-                <td>Submit Date</td>
-                <td><?= isset($pcontact)?$pcontact:'';?></td>
-              </tr>
-            </table>
-          </div>
-        </div>  
-      <?php 
-          }else{
-                echo '<p style="color:red;">Please Input Valid Roll & Email</p>';
-              }
-            }else{
-              echo '<p style="color:red;">Your Input Doesn\'t Match!</p>';
-            }
-            }else{?>
-              <script type="text/javascript">alert("Data Not Found!");</script>
-            <?php }
-          }; ?>
-    </div>
-
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="js/jquery-3.5.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+    <script src="../js/jquery-3.5.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/jquery.dataTables.min.js"></script>
+    <script src="../js/dataTables.bootstrap4.min.js"></script>
+    <script src="../js/fontawesome.min.js"></script>
+    <script src="../js/script.js"></script>
+    <title>Admin Deshboard</title>
+  </head>
+  <body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php"><i class="fas fa-chart-line fa-3x"></i></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="navbar-collapse collapse justify-content-end" id="navbarSupportedContent">
+    <?php $showuser = $_SESSION['user_login']; $haha = mysqli_query($db_con,"SELECT * FROM `users` WHERE `username`='$showuser';"); $showrow=mysqli_fetch_array($haha); ?>
+    <ul class="nav navbar-nav ">
+      <li class="nav-item"><a class="nav-link" href="index.php?page=user-profile"><i class="fa fa-user"></i> Welcome <?php echo $showrow['name']; ?>!</a></li>
+      <li class="nav-item"><a class="nav-link" href="index.php?page=add-student"><i class="fa fa-user-plus"></i> Add Stuennt</a></li>
+      <li class="nav-item"><a class="nav-link" href="index.php?page=user-profile"><i class="fa fa-user"></i> Profile</a></li>
+      <li class="nav-item"><a class="nav-link" href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+    </ul>
+  </div>
+</nav>
+<br>
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3">
+            <div class="list-group">
+              <a href="index.php?page=dashboard" class="list-group-item list-group-item-action active">
+               <i class="fas fa-tachometer-alt"></i> Dashboard
+              </a>
+              <a href="index.php?page=add-student" class="list-group-item list-group-item-action"><i class="fa fa-user-plus"></i> Add Student</a>
+              <a href="index.php?page=all-student" class="list-group-item list-group-item-action"><i class="fa fa-users"></i> All Students</a>
+              <a href="index.php?page=all-users" class="list-group-item list-group-item-action"><i class="fa fa-users"></i> All Users</a>
+              <a href="index.php?page=user-profile" class="list-group-item list-group-item-action"><i class="fa fa-user"></i> User Profile</a>
+            </div>
+          </div>
+          <div class="col-md-9">
+             <div class="content">
+                 <?php 
+                   if (isset($_GET['page'])) {
+                    $page = $_GET['page'].'.php';
+                    }else{
+                      $page = 'dashboard.php';
+                    }
+
+                    if (file_exists($page)) {
+                      require_once $page;
+                    }else{
+                      require_once '404.php';
+                    }
+                  ?>
+             </div>
+        </div>
+        </div>  
+    </div>
+    <div class="clearfix"></div>
+    <footer>
+      <div class="container">
+      <p>Copyright 2022 &copy; Lester Cueva|MSIT 508 </p>
+      </div>
+    </footer>
+    <script type="text/javascript">
+      jQuery('.toast').toast('show');
+    </script>
   </body>
 </html>
